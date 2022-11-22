105c038c65e395563890ab05bb63cb46305ca8f5
Vishvas Patel <pvishvas7@gmail.com>
2022-11-22 19:17:38 +0530

    easy question added

---
+++ src/leetcode/easy_1812.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class easy_1812 {
+    public static void main(String[] args) {
+        String a = "b1";
+        System.out.println(squareIsWhite(a));
+    }
+    public static boolean squareIsWhite(String coordinates) {
+
+           return (((int)coordinates.charAt(0)%97)+coordinates.charAt(1))%2 == 0;  
+            }
+       
+    }
+
+
