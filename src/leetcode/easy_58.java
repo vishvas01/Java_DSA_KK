c691fb59853b7a2a67c5f4aaee8181910b4b1e95
Vishvas Patel <pvishvas7@gmail.com>
2022-11-24 12:09:53 +0530

    one more leet code q added

---
+++ src/leetcode/easy_58.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+class Solution {
+    public static void main(String[] args) {
+        
+    }
+    public int lengthOfLastWord(String s) {
+        {
+        int i = 0;
+        int flag = 1;
+        s=s.trim();
+
+        for (i= s.lastIndexOf(" ")+1; i<s.length()-1;i++) {
+            flag++;
+        }
+        return flag;
+    }
+}
+}
