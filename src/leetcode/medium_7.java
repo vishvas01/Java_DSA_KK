dc2f26e41851801e91a9fc5a57ac96a1e5deccfe
Vishvas Patel <pvishvas7@gmail.com>
2022-11-24 18:53:46 +0530

    med question added

---
+++ src/leetcode/medium_7.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+class Solution {
+    public static void main(String[] args) {
+        
+    }
+    public int reverse(int x) {
+        boolean flag= false;
+        if(x<0){
+            flag = true;
+            x=x*-1;
+        }
+        long rev=0;
+        while(x>0){
+            rev = rev*10+x%10;
+            x/=10;
+        }
+        if (rev>Integer.MAX_VALUE){
+            return 0;
+        }
+        if(flag)return (int)rev*-1;
+        return (int)rev;
+        // String[] rev={};
+        // if(x.charAt(0)=='-'){
+        //     rev[0]='-';
+        //     for(int i=x.length()-1;i>=1;i--){
+        //         rev[x.length()-i]= x.charAt(i);
+        //     }
+        // }else{
+        //     for(int i=x.length()-1;i>=1;i--){
+        //         rev[x.length()-1-i]= x.charAt(i);
+        //     }
+        // }
+        // return rev;
+    }
+}
