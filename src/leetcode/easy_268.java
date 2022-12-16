121d5a4b3456351fa721d54015e8b476519a7d54
Vishvas Patel <pvishvas7@gmail.com>
2022-12-16 17:05:03 +0530

    updated 268

--- src/leetcode/easy_268.java
+++ src/leetcode/easy_268.java
@@ -1,7 +1,15 @@
 package leetcode;
+
+import java.util.Arrays;
+
 //https://leetcode.com/problems/missing-number/
 public class easy_268 {
-    public int missingNumber(int[] arr) {
+    public static void main(String[] args) {
+        int[] arr = {3,5,4,1,0};
+       System.out.println(missingNumber(arr));
+    //    System.out.println(Arrays.toString(arr));
+    }
+    static int missingNumber(int[] arr) {
         for(int i=0;i<arr.length;i++){
             while((i)!=arr[i]&&arr[i]<arr.length){
                 int temp =arr[arr[i]];
