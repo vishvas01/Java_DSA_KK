e3de099e6bf366ee239a69e0c1ae64a13c54770f
Vishvas Patel <pvishvas7@gmail.com>
2023-01-07 14:20:18 +0530

    set ith bitwise

---
+++ src/bit_wise/set_ithBit.java
@@ -0,0 +1,9 @@
+package bit_wise;
+
+public class set_ithBit {
+    public static void main(String[] args) {
+        int bit= 10001101;
+        //setting 5th bit as 1
+        System.out.println(bit|(1<<4));
+    }
+}
