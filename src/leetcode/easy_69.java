6cb12006ed157c48b6e027eb49ad3b0f8489b631
Vishvas Patel <pvishvas7@gmail.com>
2023-01-10 14:01:04 +0530

    easy 69 added

--- src/leetcode/easy_69.java
+++ src/leetcode/easy_69.java
@@ -2,23 +2,21 @@ package leetcode;
 
 public class easy_69 {
     public int mySqrt(int x) {
-        int ans=0;
-        int start=1;
-        int end=x;
-        while(start<end){
-            int mid = (start + end)/2;
-            if(mid*mid == x){
+        int s = 1;
+        int e = x;
+
+        while(s <= e){
+            int mid =s+ (e-s) / 2;
+
+            if(x / mid == mid){
                 return mid;
-            }
-            if(mid*mid > x){
-                end = mid;
-                ans=mid;
-            }
-            if(mid*mid <x){
-                start = mid;
-                ans=mid;
+            } else if(mid > x / mid){
+                e = mid - 1;
+            } else {
+                s = mid + 1;
             }
         }
-        return ans;
+
+        return e;
     }
 }
