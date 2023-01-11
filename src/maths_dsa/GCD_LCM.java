ac97870d19366fa9e4f97ae577655b8579596cf2
Vishvas Patel <pvishvas7@gmail.com>
2023-01-11 14:02:42 +0530

    gcd recurrsion added

---
+++ src/maths_dsa/GCD_LCM.java
@@ -0,0 +1,14 @@
+package maths_dsa;
+
+public class GCD_LCM {
+    public static void main(String[] args) {
+        System.out.println(gcd(4, 16));
+    }
+
+    static int gcd(int a,int b){
+        if(a==0){
+            return b;
+        }
+        return gcd(b%a, a);
+    }
+}
