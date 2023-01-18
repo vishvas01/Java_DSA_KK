bf791b39f6720fb5d1fd98af4dbe0ef901113fd1
Vishvas Patel <pvishvas7@gmail.com>
2023-01-18 22:19:08 +0530

    sum of digit added

---
+++ src/recursion/sum_ofDigits.java
@@ -0,0 +1,14 @@
+package recursion;
+
+public class sum_ofDigits {
+    public static void main(String[] args) {
+        System.out.println(sum(1234));
+    }
+
+    static int sum(int n){
+        if(n<=0){
+            return 0;
+        }
+        return n%10+sum(n/10);
+    }
+}
