13d5b75a930da951297c93456ce9cb54377bc308
Vishvas Patel <pvishvas7@gmail.com>
2023-01-28 19:34:53 +0530

    updated

--- src/leetcode/easy_58.java
+++ src/leetcode/easy_58.java
@@ -4,16 +4,16 @@ class Solution {
     public static void main(String[] args) {
         
     }
-    public int lengthOfLastWord(String s) {
-        {
-        int i = 0;
-        int flag = 1;
-        s=s.trim();
+    // public int lengthOfLastWord(String s) {
+    //     {
+    //     int i = 0;
+    //     int flag = 1;
+    //     s=s.trim();
 
-        for (i= s.lastIndexOf(" ")+1; i<s.length()-1;i++) {
-            flag++;
-        }
-        return flag;
+    //     for (i= s.lastIndexOf(" ")+1; i<s.length()-1;i++) {
+    //         flag++;
+    //     }
+    //     return flag;
     }
 }
 }
