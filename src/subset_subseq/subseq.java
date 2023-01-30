d2569f7ea9b99b242b4bdb5f1963a93acc1d2c97
Vishvas Patel <pvishvas7@gmail.com>
2023-01-30 18:36:35 +0530

    subsequence in list added

--- src/subset_subseq/subseq.java
+++ src/subset_subseq/subseq.java
@@ -1,8 +1,10 @@
 package subset_subseq;
+import java.util.ArrayList;
+
 
 public class subseq {
     public static void main(String[] args) {
-        subsequences("", "abc");
+        System.out.println(subseqList("", "abc"));
     }
 
     static void subsequences(String ans,String word){
@@ -12,5 +14,17 @@ public class subseq {
         }
         subsequences(ans, word.substring(1));
         subsequences(ans+ word.charAt(0), word.substring(1));        
+    }
+
+    static ArrayList<String> subseqList(String ans, String word){
+        if(word.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(ans);
+            return list;
+        }
+        ArrayList<String> left = subseqList(ans,word.substring(1));
+        ArrayList<String> right = subseqList(ans+word.charAt(0),word.substring(1));
+        left.addAll(right);
+        return left;
     }
 }
