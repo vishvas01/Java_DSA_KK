785d369af057708c57e86072be48404107f25948
Vishvas Patel <pvishvas7@gmail.com>
2023-01-30 14:53:32 +0530

    character removal from string using recursion added

--- src/recursion/remove_char_fromString.java
+++ src/recursion/remove_char_fromString.java
@@ -3,10 +3,10 @@ package recursion;
 public class remove_char_fromString {
     public static void main(String[] args) {
         String word = "abbcdacdac";
-        remove("", word);
+        System.out.println(remove2( word));
     }
 
-    static void remove(String ans, String word){
+    static void remove1(String ans, String word){
         if(word.isEmpty()){
             System.out.println(ans);
             return;
@@ -14,9 +14,21 @@ public class remove_char_fromString {
 
         char ch = word.charAt(0);
         if(ch=='a'){
-            remove(ans, word.substring(1));
+            remove1(ans, word.substring(1));
         }else{
-            remove(ans+ch, word.substring(1));
+            remove1(ans+ch, word.substring(1));
+        }
+    }
+
+    static String remove2(String word){
+        if(word.isEmpty()){
+            return "";
+        }
+        char ch = word.charAt(0);
+        if(ch=='a'){
+            return remove2(word.substring(1));
+        }else{
+            return ch+remove2(word.substring(1));
         }
     }
 }
