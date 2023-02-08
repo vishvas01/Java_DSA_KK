3a5c37aea56ebfe63114b68f605d569186cba28f
Vishvas Patel <pvishvas7@gmail.com>
2023-02-08 19:48:14 +0530

    diagonal path maze added

--- src/back_tracking/maze_path.java
+++ src/back_tracking/maze_path.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class maze_path {
     public static void main(String[] args) {
-        System.out.println(countlist("", 3, 3));
+        System.out.println(countlist_diag("", 3, 3));
     }
     static void count(String ans,int row,int col){
         if(row==1 && col==1){
@@ -37,6 +37,27 @@ public class maze_path {
         }
         if(col>1){
             right.addAll(countlist(ans+"R",row, col-1));
+        }
+        right.addAll(down);
+        return right;
+    }
+    static ArrayList<String> countlist_diag(String ans,int row,int col){
+        if(row==1 && col==1){
+            ArrayList<String> path= new ArrayList<>();
+            path.add(ans);
+            return path;
+        }
+        // int count=0;
+        ArrayList<String> down= new ArrayList<>();
+        ArrayList<String> right= new ArrayList<>();
+        if(row>1&&col>1){
+            down.addAll(countlist_diag(ans+"c",row-1,col-1));
+        }
+        if(row>1){
+           down.addAll(countlist_diag(ans+"D",row-1,col));
+        }
+        if(col>1){
+            right.addAll(countlist_diag(ans+"R",row, col-1));
         }
         right.addAll(down);
         return right;
