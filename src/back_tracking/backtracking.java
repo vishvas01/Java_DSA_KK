bc4514b26e75d6ce8e090d38b037efca8caf163b
Vishvas Patel <pvishvas7@gmail.com>
2023-02-09 19:53:58 +0530

    path print matrix added

--- src/back_tracking/backtracking.java
+++ src/back_tracking/backtracking.java
@@ -1,4 +1,7 @@
 package back_tracking;
+
+import java.util.Arrays;
+
 public class backtracking{
     public static void main(String[] args) {
         boolean[][] maze = {
@@ -6,7 +9,8 @@ public class backtracking{
             {true,true,true},
             {true,true,true}
         };
-        pathbacktracking("", maze, 0, 0);
+        int[][] path = new int[maze.length][maze[0].length];
+        pathbacktrackingprint("", maze, 0, 0,path,1);
     }
 
     static void pathbacktracking(String ans,boolean[][] map,int r,int c){
@@ -32,5 +36,37 @@ public class backtracking{
             pathbacktracking(ans+"L", map, r, c-1);
         }
         map[r][c]= true;
+    }
+
+    static void pathbacktrackingprint(String ans,boolean[][] map,int r,int c,int[][] path,int step){
+        if(r==map.length-1&&c==map[0].length-1){
+            path[r][c]= step;
+            for(int[] arr:path){
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(ans);
+            System.out.println();
+            return;
+        }
+
+        if(!map[r][c]){
+            return;
+        }
+        map[r][c]= false;
+        path[r][c]= step;
+        
+        if(r<map.length-1){
+            pathbacktrackingprint(ans+"D", map, r+1, c,path,step+1);
+        }if(c<map[0].length-1){
+            pathbacktrackingprint(ans+"R", map, r, c+1,path,step+1);
+        }
+        if(r>0){
+            pathbacktrackingprint(ans+"U", map, r-1, c,path,step+1);
+        }
+        if(c>0){
+            pathbacktrackingprint(ans+"L", map, r, c-1,path,step+1);
+        }
+        map[r][c]= true;
+        path[r][c]= 0;
     }
 }
