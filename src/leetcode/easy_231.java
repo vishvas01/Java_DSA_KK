c1d3a4a48715099c588d63b19c0308b90d42c45a
Vishvas Patel <pvishvas7@gmail.com>
2023-03-13 18:43:59 +0530

    added easy 231

---
+++ src/leetcode/easy_231.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        return power(n);
+    }
+
+    boolean power(int n){
+        
+        if(n==1){
+            return true;
+        }
+        if(n%2!=0 || n<=0){
+            return false;
+        }
+        return power(n/2);
+    }
+}
