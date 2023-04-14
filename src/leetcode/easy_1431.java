d7d3b0f891be733de43fafc97f57b1d41a3608bc
Vishvas Patel <pvishvas7@gmail.com>
2023-04-14 23:44:21 +0530

    easy 1431 added

---
+++ src/leetcode/easy_1431.java
@@ -0,0 +1,23 @@
+package leetcode;
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+       int max=candies[0];
+       for(int i=1;i<candies.length;i++){
+           if(max<candies[i]){
+               max=candies[i];
+           }
+        }
+        List<Boolean> ans= new ArrayList<>();
+        for(int i=0;i<candies.length;i++){
+            if((candies[i]+extraCandies)>=max){
+                ans.add(true);
+            }else{
+                ans.add(false);
+            }
+        }
+        return ans;
+    }
+}
