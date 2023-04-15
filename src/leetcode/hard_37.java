156d82bdcc28aac184dad57d0b09a79d6dac43d3
Vishvas Patel <pvishvas7@gmail.com>
2023-04-15 12:23:31 +0530

    hard 37 added

--- src/leetcode/hard_37.java
+++ src/leetcode/hard_37.java
@@ -1,54 +1,85 @@
 package leetcode;
-
-
 public class hard_37 {
     public static void main(String[] args) {
         char[][] board= {
-             {'3', '1', '6', '5', '7', '8', '4', '.', '2'},
-{'5','2', '9', '1', '3', '4', '.', '6', '.'},
-{'4', '8', '7', '.', '2', '9', '.', '3', '1'},
-{'2', '6', '3', '.', '1', '5', '9', '8', '7'},
-{'9', '.', '4', '8', '6', '3', '1', '2', '5'},
-{'.', '5', '1', '7', '9', '2', '6', '4', '3'}, 
-{'1', '3', '.', '9', '4', '7', '2', '5', '6'},
-{'6', '9', '2', '3', '5', '1', '.', '7', '4'},
-{'7', '4', '5', '2', '8', '6', '3', '.', '9'}    
+            {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+            {'6','.', '.', '1', '9', '5', '.', '.', '.'},
+            {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+            {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+            {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+            {'7', '.', '.', '.', '2', '.', '.', '.', '6'}, 
+            {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+            {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+            {'.', '.', '.', '.', '8', '.', '.', '7', '9'}  
+//              {'3', '1', '6', '5', '7', '8', '4', '.', '2'},
+// {'5','2', '9', '1', '3', '4', '.', '6', '.'},
+// {'4', '8', '7', '.', '2', '9', '.', '3', '1'},
+// {'2', '6', '3', '.', '1', '5', '9', '8', '7'},
+// {'9', '.', '4', '8', '6', '3', '1', '2', '5'},
+// {'.', '5', '1', '7', '9', '2', '6', '4', '3'}, 
+// {'1', '3', '.', '9', '4', '7', '2', '5', '6'},
+// {'6', '9', '2', '3', '5', '1', '.', '7', '4'},
+// {'7', '4', '5', '2', '8', '6', '3', '.', '9'}    
     }; solveSudoku(board);
 }
     public static void solveSudoku(char[][] board) {
-        if(checker(board, 0, 0)){
+        if(checker(board)){
             display(board);
         }else{
             System.out.println("cannot solve");
         }
     }
 
-    public static boolean checker(char[][] board,int r,int c){
-        if(r==board.length-1&&c==board.length){
-            return true;
-        }
-        if(c==board.length){
-            r = r+1;
-            c=0;    
-        }
-        for(int i=r;i<board.length;i++){
-            for(int j=c;j<board.length;j++){
+    public static boolean checker(char[][] board){
+        // if(r==board.length){
+        //     return true;
+        // }
+        // if(c==board.length){
+        //     r = r+1;
+        //     c=0;    
+        // }
+        boolean empty=false;
+        int r=-2,c=-1;
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++){
                 if(board[i][j]=='.'){
-                    for(char digit='1';digit<='9';digit++){
-                        if(isSafe(board,i,j,digit)){
-                            board[i][j]=digit;
-                           if(checker(board, r, c+1)){
-                            return true;
-                           }else{
-                            board[i][j]='.';
-                            // checker(board, r, c+1);
-                           }
-                        }
-                    }
+                    // for(char digit='1';digit<='9';digit++){
+                    //     if(isSafe(board,i,j,digit)){
+                    //         board[i][j]=digit;
+                    //        if(checker(board, r, c+1)){
+                    //         return true;
+                    //        }else{
+                    //         board[i][j]='.';
+                    //         //checker(board, r, c+1);
+                    //        }
+                    //     }
+                    // }
+                    r=i;
+                    c=j;
+                    empty=true;
+                    break;
                 }
             }
+            if(empty==true){
+                break;
+            }
         }
-        return checker(board, r, c+1);
+        if(!empty){
+            return true;
+        }
+
+        for(char digit='1';digit<='9';digit++){
+            if(isSafe(board,r,c,digit)){
+                board[r][c]=digit;
+               if(checker(board)){
+                return true;
+               }else{
+                board[r][c]='.';
+                //checker(board, r, c+1);
+               }
+            }
+        }
+        return false;
     }
 
     public static void display(char[][] board) {
