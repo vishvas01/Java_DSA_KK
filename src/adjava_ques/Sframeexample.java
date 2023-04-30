240b369a29cc3e9926e1b4cc8c6250d13d5dc23f
Vishvas Patel <pvishvas7@gmail.com>
2023-04-30 21:20:59 +0530

    hard 1579 added

---
+++ src/adjava_ques/Sframeexample.java
@@ -0,0 +1,10 @@
+package adjava_ques;
+import javax.swing.*;
+public class Sframeexample{
+    public static void main(String[] args) {
+        Jframe f= new Jframe("Lord Vishvas welcomes you!");
+        f.setSize(400,500);
+        f.setVisible(true);
+
+    }
+}
