4241719862fbaa3677fbe71087a2b7e345a344a2
Vishvas Patel <pvishvas7@gmail.com>
2023-06-27 21:18:39 +0530

    easy 119 added

--- src/leetcode/easy_119.java
+++ src/leetcode/easy_119.java
@@ -22,22 +22,29 @@
 
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-          int s = 1;
-        if (rowIndex == 0) {
-            ArrayList<Integer> res = new ArrayList<>();
-            res.add(1);
-            return res;
-        }
-        
         ArrayList<Integer> ans = new ArrayList<>();
-        ans.add(1);
-        ans.add(1);
-        
-        for (int i = 1; i < rowIndex; i++) {
-            s = s * (rowIndex - i + 1) / i;
-            ans.add(i, s);
+        ans.add(1); // First element is always 1
+        for (int i = 1; i <= rowIndex; i++) {
+            long curr = (long) ans.get(i - 1) * (rowIndex - i + 1) / i;
+            ans.add((int) curr);
         }
-        
         return ans;
+        //   int s = 1;
+        // if (rowIndex == 0) {
+        //     ArrayList<Integer> res = new ArrayList<>();
+        //     res.add(1);
+        //     return res;
+        // }
+        
+        // ArrayList<Integer> ans = new ArrayList<>();
+        // ans.add(1);
+        // ans.add(1);
+        
+        // for (int i = 1; i < rowIndex; i++) {
+        //     s = s * (rowIndex - i + 1) / i;
+        //     ans.add(i, s);
+        // }
+        
+        // return ans;
     }
 }
