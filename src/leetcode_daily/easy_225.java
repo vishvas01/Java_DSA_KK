67b641c4d0325d6357f0d7729da91aafde61486f
Vishvas Patel <pvishvas7@gmail.com>
2023-08-28 23:53:32 +0530

    easy 225 added

---
+++ src/leetcode_daily/easy_225.java
@@ -0,0 +1,35 @@
+package leetcode_daily;
+
+public class easy_225 {
+    
+}
+
+class MyStack {
+    private Queue<Integer> q = new LinkedList<>();
+
+    MyStack() {
+        
+    }
+
+    void push(int x) {
+        q.add(x);
+        int v = q.size() - 1;
+        int i = 0;
+        while (i < v) {
+            q.add(q.poll());
+            i++;
+        }
+    }
+
+    int pop() {
+        return q.poll();
+    }
+
+    int top() {
+        return q.peek();
+    }
+
+    boolean empty() {
+        return q.isEmpty();
+    }
+}
