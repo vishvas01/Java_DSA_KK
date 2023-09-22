d39a4364f4ee261662f16309ad267f410850ba74
Vishvas Patel <pvishvas7@gmail.com>
2023-09-22 23:59:23 +0530

    easy 392 added

---
+++ src/leetcode_daily/easy_392.java
@@ -0,0 +1,20 @@
+package leetcode_daily;
+
+public class easy_392 {
+    
+}
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int sIdx = 0;
+        int tIdx = 0;
+
+        while (sIdx < s.length() && tIdx < t.length()) {
+            if (s.charAt(sIdx) == t.charAt(tIdx)) {
+                sIdx++;
+            }
+            tIdx++;
+        }
+
+        return sIdx == s.length();        
+    }
+}
