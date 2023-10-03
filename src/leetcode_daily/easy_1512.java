16687a7d46fbc64fe62399fd4f40b47874a54dd2
Vishvas Patel <pvishvas7@gmail.com>
2023-10-03 15:50:50 +0530

    easy 1512 added

---
+++ src/leetcode_daily/easy_1512.java
@@ -0,0 +1,11 @@
+package leetcode_daily;
+
+class Solution {
+        public int numIdenticalPairs(int[] A) {
+        int ans = 0, cnt[] = new int[101];
+        for (int a: A) {
+            ans += cnt[a]++;
+        }
+        return ans;
+    }
+}
