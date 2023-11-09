24eb948fad94ec70a7c1c048b9d2a2c5fa67a1e8
Vishvas Patel <pvishvas7@gmail.com>
2023-11-09 21:24:40 +0530

    medium 1759 added

---
+++ src/leetcode_daily/medium_1759.java
@@ -0,0 +1,20 @@
+public class medium_1759 {
+    
+}
+class Solution {
+    public int countHomogenous(String s) {
+        int left = 0;
+        long res = 0;
+        
+        for (int right = 0; right < s.length(); right++) {
+            if (s.charAt(left) == s.charAt(right)) {
+                res += right - left + 1;
+            } else {
+                res += 1;
+                left = right;
+            }
+        }
+
+        return (int) (res % (1000000007));       
+    }
+}
