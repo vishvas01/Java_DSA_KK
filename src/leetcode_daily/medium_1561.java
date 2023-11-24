6a08e8f65b36cb9b6dba250d187ac7737970efae
Vishvas Patel <pvishvas7@gmail.com>
2023-11-24 12:19:21 +0530

    medium 1561 added

---
+++ src/leetcode_daily/medium_1561.java
@@ -0,0 +1,16 @@
+public class medium_1561 {
+    
+}
+class Solution {
+    public int maxCoins(int[] piles) {
+        Arrays.sort(piles);
+        int ans = 0;
+        int n = piles.length;
+
+        for (int i = n / 3; i < n; i += 2) {
+            ans += piles[i];
+        }
+
+        return ans;
+    }
+}
