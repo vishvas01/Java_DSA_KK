80c1db484b6328ca0772422bdefedcd6a04aaf62
Vishvas Patel <pvishvas7@gmail.com>
2023-11-29 17:14:48 +0530

    easy 191 added

---
+++ src/leetcode_daily/easy_191.java
@@ -0,0 +1,14 @@
+package leetcode_daily;
+
+class Solution {
+    public int hammingWeight(int n) {
+		int setBitCount = 0;
+        
+		while (n != 0) {
+            n &= (n - 1);
+            ++setBitCount;
+        }
+		
+        return setBitCount;
+    }
+}
