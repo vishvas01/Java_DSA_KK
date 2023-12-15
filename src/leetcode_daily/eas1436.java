aa9b14d6d0a63aef684305637247c76c80b727c9
Vishvas Patel <pvishvas7@gmail.com>
2023-12-15 21:31:00 +0530

    easy 1436  added

---
+++ src/leetcode_daily/eas1436.java
@@ -0,0 +1,18 @@
+package leetcode_daily;
+
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        Set<String> cities = new HashSet<>(); 
+        for (List<String> path : paths) {
+            cities.add(path.get(0)); 
+        }
+        
+        for (List<String> path : paths) {
+            String dest = path.get(1); 
+            if (!cities.contains(dest)) {
+                return dest; 
+            }
+        }
+        return "";
+    }
+}
