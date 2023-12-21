2352f532804ed05adbd15bcc6e50a9a2ef1637b0
Vishvas Patel <pvishvas7@gmail.com>
2023-12-21 18:05:33 +0530

    medium 1637 added

---
+++ src/leetcode_daily/medium_1637.java
@@ -0,0 +1,16 @@
+package leetcode_daily;
+
+import java.util.Arrays;
+
+class Solution {
+    public int maxWidthOfVerticalArea(int[][] points) {
+        Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
+        int diff = 0;
+        for (int i = 1; i < points.length; i++) {
+            diff = Math.max(diff, points[i][0] - points[i - 1][0]);
+        }
+        return diff;
+    }
+}
+
+
