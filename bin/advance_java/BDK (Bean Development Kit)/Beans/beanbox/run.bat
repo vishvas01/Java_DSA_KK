45ec76f51acdb957c33f98873f18c4d9044b2945
Vishvas Patel <pvishvas7@gmail.com>
2023-12-22 21:35:42 +0530

    easy 1422 added

--- bin/advance_java/BDK (Bean Development Kit)/Beans/beanbox/run.bat
+++
@@ -1,3 +0,0 @@
-if "%OS%" == "Windows_NT" setlocal
-set CLASSPATH=classes;..\lib\methodtracer.jar;..\infobus.jar
-java sun.beanbox.BeanBoxFrame
--- bin/advance_java/BDK (Bean Development Kit)/Beans/beanbox/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export CLASSPATH
-CLASSPATH=classes:../infobus.jar:../lib/methodtracer.jar
-java sun.beanbox.BeanBoxFrame
